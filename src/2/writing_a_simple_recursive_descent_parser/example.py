8ebd97daa0eea3d6eeb30983ed1fb34e54b68487
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-07-09 16:51:33 +0800

    adding trace

--- src/2/writing_a_simple_recursive_descent_parser/example.py
+++ src/2/writing_a_simple_recursive_descent_parser/example.py
@@ -1,7 +1,45 @@
 # example.py
 #
 # An example of writing a simple recursive descent parser
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+import re
+import functools
+PREFIX = ''
+def trace(fn):
+    """A decorator that prints a function's name, its arguments, and its return
+    values each time the function is called. For example,
 
+    @trace
+    def compute_something(x, y):
+        # function body
+    """
+    @functools.wraps(fn)
+    def wrapped(*args, **kwds):
+        global PREFIX
+        reprs = [repr(e) for e in args] 
+        reprs += [repr(k) + '=' + repr(v) for k, v in kwds.items()]
+        log('{0}({1})'.format(fn.__name__, ', '.join(reprs)) + ':')
+        PREFIX += '    '
+        try:
+            result = fn(*args, **kwds)
+            PREFIX = PREFIX[:-4]
+        except Exception as e:
+            log(fn.__name__ + ' exited via exception')
+            PREFIX = PREFIX[:-4]
+            raise
+        # Here, print out the return value.
+        #log('{0}({1}) -> {2}'.format(fn.__name__, ', '.join(reprs), result))
+        log('-> {0}'.format(result,))
+        return result
+    return wrapped
+    
+def log(message):
+    """Print an indented message (used with trace)."""
+    if type(message) is not str:
+        message = str(message)
+    print(PREFIX + re.sub('\n', '\n' + PREFIX, message))
+    
 import re
 import collections
 
@@ -38,6 +76,7 @@ class ExpressionEvaluator:
     (or raise a SyntaxError if it doesn't match).
     '''
 
+    @trace
     def parse(self,text):
         self.tokens = generate_tokens(text)
         self.tok = None             # Last symbol consumed
